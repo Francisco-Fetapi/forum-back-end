050b8c60809a21082a0790deef21d10ee5f3b0f4
Francisco Fetapi <franciscofetapi10@gmail.com>
2021-12-31 04:12:25 +0000

    preparando para deploy2

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
-    require_once './autoload.php';
-    require_once './Controllers/Controllers.php';
+require_once './autoload.php';
+require_once './Controllers/Controllers.php';
 
-    header("Content-Type: application/json");
-    header("Access-Control-Allow-Origin:*"); 
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin:*");
 
-    $controller = $_GET['controller'] ?? $_POST['controller'];
-    $funcao = $_GET['funcao'] ?? $_POST['funcao'];
+$controller = $_GET['controller'] ?? $_POST['controller'];
+$funcao = $_GET['funcao'] ?? $_POST['funcao'];
 
-    if($controller && $funcao){
-        require_once "Controllers/$controller.php";
-    }else{
-        echo "<h1>Forum - Pagina Inicial</h1>";
-    }
+if ($controller || $funcao) {
+    require_once "/app/Controllers/$controller.php";
+} else {
+    echo "Forum - Pagina Inicial";
+}
